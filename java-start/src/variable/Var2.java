3a68c1e858b61389b26766fb768be5f99af5d683
yhg1024 <dgr1024kr@naver.com>
2024-06-13 23:40:27 +0900

    변수

--- java-start/src/variable/Var2.java
+++ java-start/src/variable/Var2.java
@@ -4,7 +4,7 @@ public class Var2 {
     public static void main(String[] args) {
         // 변수 : 값을 꺼내서 읽을 수 있는 저장소
         int a; // 변수 선수
-        a = 10; // 변수 초기화
+        a = 20; // 변수 초기화
 
         System.out.println(a);
         System.out.println(a);
