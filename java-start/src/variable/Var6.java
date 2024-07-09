a6c3e05d5d8eb60493806535b0b3fa9f943ea34f
yhg1024 <dgr1024kr@naver.com>
2024-07-09 23:42:28 +0900

    변수선언과 초기화

--- java-start/src/variable/Var6.java
+++ java-start/src/variable/Var6.java
@@ -4,6 +4,6 @@ public class Var6 {
     public static void main(String[] args) {
         // 초기화 하지 않고 실행하면 컴파일 에러 발생
         int a;
-        System.out.println(a);
+        // System.out.println(a);
     }
 }
