dada1594ec369721b3a1ef52f55151222849ecab
yhg1024 <dgr1024kr@naver.com>
2024-07-09 23:33:57 +0900

    변수선언과 초기화

--- java-start/src/variable/Var5.java
+++ java-start/src/variable/Var5.java
@@ -10,5 +10,10 @@ public class Var5 {
         // 2. 변수 선언과 초기화를 한번에
         int b = 2;
         System.out.println(b);
+
+        // 3. 여러 변수 선언과 초기화를 한번에
+        int c = 3, d = 4;
+        System.out.println(c);
+        System.out.println(d);
     }
 }
