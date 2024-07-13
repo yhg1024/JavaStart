469d9cb2e525c9394523f49e842e16ae192b0f0b
yhg1024 <dgr1024kr@naver.com>
2024-07-13 23:44:47 +0900

    변수타입

---
+++ java-start/src/variable/Var7.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        // 변수는 데이터를 다루는 종류에 따라 다양한 형식(type)이 존재한다.
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언(boolean) true, false 입력가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별한 타입
+
+        // 자신의 타입에 맞는 데이터를 사용
+        // int z = "백원"; // 정수 타입의 문자열(x)
+
+        // 리터럴 : 코드에서 개발자가 직접 적은 100, 10.5와 같은 고정된 값
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
